7b1d1e090f9298efc597ebaafde64ef65cdeddcc
Yae Miko <aloksinghkumar756@gmail.com>
2025-11-15 14:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7938087417:AAHlOKaw_XPTlfET_qwfq2USmnFs
 ADMIN = int(os.environ.get("ADMIN", "7534267467"))
 PICS = (os.environ.get("PICS", "https://graph.org/file/075bbc8002a19e76d99b5-59bcbc49f2d7c2e0b5.jpg")).split()
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002979722645"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://ZeroTwo:aloksingh@zerotwo.3q3ij.mongodb.net/?retryWrites=true&w=majority")
